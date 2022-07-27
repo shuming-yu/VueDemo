54abbc851e56de69d83ba8653cd33565211adbd8
Ben <yu.shuming0829@gmail.com>
2022-07-27 16:36:15 +0800

    first

---
+++ README.md
@@ -0,0 +1,4 @@
+## 相關資源
+
+### HomePage : https://shuming-yu.github.io/VueDemo/dist/#/
+### Page : https://shuming-yu.github.io/VueDemo/dist/#/login
---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>  
+  </nav> -->
+  <!-- <button type="button" class="btn btn-primary">Primary</button>
+  <button type="button" class="btn btn-secondary">Secondary</button>
+  <button type="button" class="btn btn-success">Success</button>
+  <button type="button" class="btn btn-danger">Danger</button>
+  <button type="button" class="btn btn-warning">Warning</button>
+  <button type="button" class="btn btn-info">Info</button>
+  <button type="button" class="btn btn-light">Light</button>
+  <button type="button" class="btn btn-dark">Dark</button>
+  <button type="button" class="btn btn-link">Link</button> -->
+  <!-- bootstrap classname 版面間距 -->
+  <!-- <div class="container-fluid">
+  </div> -->
+  <router-view/>  <!-- 頁面切換 -->
+</template>
+
+<style lang="scss">
+// Bootstrap 六角學院 : https://bootstrap5.hexschool.com/
+// @import "~bootstrap/scss/bootstrap";
+@import "./assets/all";
+</style>
